981f32ebea7697e82502c96ec3e508f878822c42
joaoleandro <new.joaoleandro@gmail.com>
2024-08-31 21:02:39 -0300

    feat: adicionado biblioteca axios e configurada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
+"use client"
+import { api } from "@/services/api";
 import Container from "@/components/Container"
 import BannerHome from "@/components/others/BannerHome";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [products, setProducts] = useState([]);
+
+  const fetchData = async () => {
+
+      const response = await api.get('/list-products');
+      return setProducts(response.data);
+
+};
+
+fetchData()
+
+
   return (
     <Container>
       <section>
         <BannerHome/>
+      </section>
+      <section className="mt-5">
+        {products.map((items: any) => (
+          <div key={items.id}>
+            <p>{items.nomeProduto} - {items.quantidade}</p>
+          </div>
+        ))}
       </section>
     </Container>
   );
