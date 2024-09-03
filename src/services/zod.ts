ec41ee151d5e251d8df0edebba7bed766e299033
joaoleandro <new.joaoleandro@gmail.com>
2024-09-02 21:31:13 -0300

    feat: adicionado formatações do zod, metodo para registrar usuario criado, faltam ajustes

---
+++ src/services/zod.ts
@@ -0,0 +1,8 @@
+import { z } from "zod"
+
+export const RegisterSchema = z.object({
+    nameUser: z.string().min(3, {message: "Insira um nome válido."}),
+    email: z.string().email({message: "Informe um email válido."}),
+    password: z.string().min(6, {message: "A senha deve ter no minímo 6 caracteres."}),
+    password1: z.string().min(6, {message: "A senha deve ter no minímo 6 caracteres."})
+})
